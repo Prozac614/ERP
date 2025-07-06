fc7cbec9c1d12932ea270d03192ded63ff895773
614707404 <614707404@qq.com>
2025-07-06 20:57:33 +0800

    Feat:修复结存数据

--- jshERP-boot/src/main/java/com/jsh/erp/utils/PeriodUtil.java
+++ jshERP-boot/src/main/java/com/jsh/erp/utils/PeriodUtil.java
@@ -7,24 +7,34 @@ public class PeriodUtil {
     
     /**
      * 获取当前期间的开始和结束时间
+     * 期间定义：2月1日-7月31日为一期，8月1日-次年1月31日为一期
      */
     public static String[] getCurrentPeriod() {
         LocalDate now = LocalDate.now();
         int year = now.getYear();
         int month = now.getMonthValue();
         
-        if (month <= 6) {
-            // 上半年期
+        if (month >= 2 && month <= 7) {
+            // 2-7月期间
             return new String[]{
-                year + "-01-01 00:00:00",
-                year + "-06-30 23:59:59"
+                year + "-02-01 00:00:00",
+                year + "-07-31 23:59:59"
             };
         } else {
-            // 下半年期
-            return new String[]{
-                year + "-07-01 00:00:00",
-                year + "-12-31 23:59:59"
-            };
+            // 8-1月期间（跨年）
+            if (month >= 8) {
+                // 8-12月，期间是当年8月1日到次年1月31日
+                return new String[]{
+                    year + "-08-01 00:00:00",
+                    (year + 1) + "-01-31 23:59:59"
+                };
+            } else {
+                // 1月，期间是上年8月1日到当年1月31日
+                return new String[]{
+                    (year - 1) + "-08-01 00:00:00",
+                    year + "-01-31 23:59:59"
+                };
+            }
         }
     }
     
@@ -36,18 +46,27 @@ public class PeriodUtil {
         int year = now.getYear();
         int month = now.getMonthValue();
         
-        if (month <= 6) {
-            // 当前是上半年，上期是去年下半年
+        if (month >= 2 && month <= 7) {
+            // 当前是2-7月期间，上期是上年8月1日到当年1月31日
             return new String[]{
-                (year - 1) + "-07-01 00:00:00",
-                (year - 1) + "-12-31 23:59:59"
+                (year - 1) + "-08-01 00:00:00",
+                year + "-01-31 23:59:59"
             };
         } else {
-            // 当前是下半年，上期是今年上半年
-            return new String[]{
-                year + "-01-01 00:00:00",
-                year + "-06-30 23:59:59"
-            };
+            // 当前是8-1月期间，上期是2-7月期间
+            if (month >= 8) {
+                // 当前8-12月，上期是当年2-7月
+                return new String[]{
+                    year + "-02-01 00:00:00",
+                    year + "-07-31 23:59:59"
+                };
+            } else {
+                // 当前1月，上期是上年2-7月
+                return new String[]{
+                    (year - 1) + "-02-01 00:00:00",
+                    (year - 1) + "-07-31 23:59:59"
+                };
+            }
         }
     }
     
@@ -58,16 +77,27 @@ public class PeriodUtil {
         int year = date.getYear();
         int month = date.getMonthValue();
         
-        if (month <= 6) {
+        if (month >= 2 && month <= 7) {
+            // 2-7月期间
             return new String[]{
-                year + "-01-01 00:00:00",
-                year + "-06-30 23:59:59"
+                year + "-02-01 00:00:00",
+                year + "-07-31 23:59:59"
             };
         } else {
-            return new String[]{
-                year + "-07-01 00:00:00",
-                year + "-12-31 23:59:59"
-            };
+            // 8-1月期间（跨年）
+            if (month >= 8) {
+                // 8-12月，期间是当年8月1日到次年1月31日
+                return new String[]{
+                    year + "-08-01 00:00:00",
+                    (year + 1) + "-01-31 23:59:59"
+                };
+            } else {
+                // 1月，期间是上年8月1日到当年1月31日
+                return new String[]{
+                    (year - 1) + "-08-01 00:00:00",
+                    year + "-01-31 23:59:59"
+                };
+            }
         }
     }
 } 
